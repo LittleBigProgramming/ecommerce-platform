a60a26bb2d840e3b1f9f16f54ad34f72bc475779
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2018-12-03 12:37:44 +0000

    refactored IsScopable for classes that can be scoped from product class for Reusability

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,28 +2,18 @@
 
 namespace App\Models;
 
-use App\Scoping\Scoper;
-use Illuminate\Database\Eloquent\Builder;
+use App\Models\Traits\IsScopable;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use IsScopable;
     /**
      * @return string
      */
     public function getRouteKeyName()
     {
         return 'slug';
-    }
-
-    /**
-     * @param Builder $builder
-     * @param array $scopes
-     * @return Builder
-     */
-    public function scopeWithScopes(Builder $builder, $scopes = [])
-    {
-        return (new Scoper(request()))->apply($builder, $scopes);
     }
 
     /**
---
+++ app/Models/Traits/IsScopable.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models\Traits;
+
+use App\Scoping\Scoper;
+use Illuminate\Database\Eloquent\Builder;
+
+trait IsScopable
+{
+    /**
+     * @param Builder $builder
+     * @param array $scopes
+     * @return Builder
+     */
+    public function scopeWithScopes(Builder $builder, $scopes = [])
+    {
+        return (new Scoper(request()))->apply($builder, $scopes);
+    }
+}
