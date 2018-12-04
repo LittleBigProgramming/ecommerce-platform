f56b8cc847913f8d8ddea2bb2492e2d044e00015
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2018-12-04 12:47:28 +0000

    show price difference between variation and base price within select dropdown

--- ecommerce-client/components/products/ProductVariation.vue
+++ ecommerce-client/components/products/ProductVariation.vue
@@ -10,8 +10,13 @@
                     <option
                         v-for="variation in variations"
                         :key="variation.id"
+                        :value="variation.id"
                     >
                         {{ variation.name }}
+
+                        <template v-if="variation.price_varies">
+                            ({{ variation.price }})
+                        </template>
                     </option>
                 </select>
             </div>
