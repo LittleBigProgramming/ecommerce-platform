22edcf5f44f102f5b0e815b42f6386591e3eda11
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-04-29 20:40:07 +0100

    add dropdown with stock count for each product variation if in stock, when switching between variations default the input back to 1

--- ecommerce-client/pages/products/_slug.vue
+++ ecommerce-client/pages/products/_slug.vue
@@ -38,9 +38,10 @@
                             <div class="field has-addons" v-if="form.variation">
                                 <div class="control">
                                     <div class="select is-fullwidth">
-                                        <select name="" id="">
-                                            <option value="">1</option>
-
+                                        <select name="" id="" v-model="form.quantity">
+                                            <option :value="x" v-for="x in parseInt(form.variation.stock_count)" :key="x">
+                                                {{ x }}
+                                            </option>
                                         </select>
                                     </div>
                                 </div>
@@ -67,6 +68,12 @@
             variation: '',
             quantity: 1
           }
+        }
+      },
+
+      watch: {
+        'form.variation' () {
+            this.form.quantity = 1
         }
       },
 
