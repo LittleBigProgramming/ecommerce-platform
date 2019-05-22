1f2c4cea32e976811f10cd18f52854a5e53f20e1
Andy Alban <20643587+LittleBigProgramming@users.noreply.github.com>
2019-05-22 09:10:23 +0100

    added Register endpoint for users on the api with validation for name, email and password. If all fields are valid creates a user in the database with hashed credentials

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,15 @@ class User extends Authenticatable implements JWTSubject
         'name', 'email', 'password',
     ];
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->password = bcrypt($user->password);
+        });
+    }
+
     /**
      * The attributes that should be hidden for arrays.
      *
